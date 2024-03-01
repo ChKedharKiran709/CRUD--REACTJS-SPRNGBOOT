3e3caa0f1bf112db8e2971a60ae42a85a36de578
ChKedharKiran709 <chkedharkiran@gmail.com>
2024-03-01 07:37:26 +0530

    Initial commit

---
+++ ems-backend/ems-backend/src/main/java/net/springproject/ems/service/EmployeeService.java
@@ -0,0 +1,16 @@
+package net.springproject.ems.service;
+
+import net.springproject.ems.dto.EmployeeDto;
+import java.util.List;
+
+public interface EmployeeService {
+    EmployeeDto createEmployee(EmployeeDto employeeDto);
+
+    EmployeeDto getEmployeeById(Long employeeId);
+
+    List<EmployeeDto> getAllEmployees();
+
+    EmployeeDto updateEmployee(Long employeeId, EmployeeDto updatedEmployee);
+
+    void deleteEmployee(Long employeeId);
+}
---
+++ ems-frontend/src/components/FooterComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FooterComponent = () => {
+  return (
+    <div>
+        <footer className='footer'>
+            <span>Copyright 2023 CH.Kedhar Kiran | ALL RIGHTS RESERVED </span>
+        </footer>
+    </div>
+  )
+}
+
+export default FooterComponent
